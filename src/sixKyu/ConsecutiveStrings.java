e0d02191398d7c4220b773a4b7eb7c345f8b47f3
DimaProhorenko <dima.study.web@gmail.com>
2023-05-05 22:06:25 +0300

    sldjf

--- src/sixKyu/ConsecutiveStrings.java
+++ src/sixKyu/ConsecutiveStrings.java
@@ -1,10 +1,15 @@
 package sixKyu;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 public class ConsecutiveStrings {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+  public static String longestConsec(String[] strArr, int k) {
+    return (k > 0 && strArr.length > 0 && k <= strArr.length) ?
+      IntStream.rangeClosed(0, strArr.length - k)
+        .mapToObj(i -> IntStream.range(0, k).mapToObj(j -> strArr[i + j]).collect(Collectors.joining()))
+        .sorted((s1, s2) -> Integer.compare(s2.length(), s1.length()))
+        .findFirst().get()
+      : "";
+  }
 }
