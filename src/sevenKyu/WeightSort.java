59b303b226859bdd64d7dd89cd2780f2019d41f0
DimaProhorenko <dima.study.web@gmail.com>
2023-05-10 11:53:08 +0300

    5kyu WeightSort

--- src/sevenKyu/WeightSort.java
+++ src/sevenKyu/WeightSort.java
@@ -1,24 +1,31 @@
 package sevenKyu;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class WeightSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		orderWeight("2000 103 123 4444 99");
+		System.out.println(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
+	}
+	
+	public static int compareWeights(String[] n1, String[] n2) {
+		int diff = getNumWeight(n1) - getNumWeight(n2);
+		return diff == 0 ? String.join("", n1).compareTo(String.join("", n2)) : diff < 0 ? -1 : 1;
+	}
+	
+	public static int getNumWeight(String[] n) {
+		return Arrays.stream(n).mapToInt(Integer::parseInt).sum();
 	}
 	
 	public static String orderWeight(String s) {
 		List<String> l = List.of(s.trim().split(" "));
-		List<Integer> result = new ArrayList<>();
-		System.out.println(l);
-		l.forEach(n -> {
-			String[] chars = n.split("");
-			int sum = Arrays.stream(chars).mapToInt(el -> Integer.parseInt(el)).sum();
-			
-		});
-		return "";
+		List<String[]> c = l.stream().map(el -> el.split("")).toList();
+		List<String[]> a = c.stream().sorted(WeightSort::compareWeights).toList();
+		
+		return a.stream().map(el -> String.join("", el)).collect(Collectors.joining(" "));
+		
 	}
 
 }
